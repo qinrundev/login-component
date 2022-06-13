39c66b2ae99e8df2ce832b71f7aa97be8dd58824
zzx123 <1085021693@qq.com>
2022-06-13 10:35:57 +0800

    fix: test

---
+++ packages/login-react2/src/componenets/LogoButton2.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+const LogoButton2: React.FC<{ logo: any, showModal: any }> = (props: any) => {
+    const [mouseEnter, setMouseEnter] = useState(false);
+    return (
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                flexDirection: 'column',
+                height: 48,
+                width: 56,
+                border: '1px solid #D4D8DD',
+                borderRadius: '10%',
+                backgroundColor: mouseEnter ? '#E8E8E8' : '#F8F8F8',
+            }}
+            onMouseEnter={() => setMouseEnter(true)}
+            onMouseLeave={() => setMouseEnter(false)}
+            onClick={() => { props.showModal(true) }}
+        >
+            {props.logo}
+        </div>
+
+    );
+};
+export default LogoButton2;
